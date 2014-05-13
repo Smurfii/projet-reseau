2de553dce1350462ee7058ebab226a3ba0cf590e
Smurfii <hugo.durchon@gmail.com>
2014-05-13 11:40:12 +0200

    Update client_projet.c

--- client_projet.c
+++ client_projet.c
@@ -11,12 +11,12 @@
 #include <errno.h>
 #include <string.h>
 
-#define BUF_LEN 65536
+#define BUF_LEN 65536			
 
 
 int lire(char *chaine, int longueur)
 {	
-/*Initialisation de la position d'entrée */
+	/*Initialisation de la position d'entrée */
     char *positionEntree = NULL;
   
     /* On lit le texte saisi au clavier */
@@ -33,7 +33,7 @@ int lire(char *chaine, int longueur)
     }
 
     else {
-        return 1; /* On renvoie 1 si la fonction s'est mal déroulée */
+        return 1; /* On renvoie 1 si la fonction s'est mal déroulée */ 
     }
 }
 
@@ -58,10 +58,10 @@ serveur_len = sizeof(serveur);
 client_len = sizeof(client);
 
 /* Vérification du nombre d'arguments */
-if (argc != 3) {
-fprintf(stderr, "Usage: %s ip_serveur, nom_fichier, port\n", argv[0]);
-exit(1);
-}
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s ip_serveur, nom_fichier, port\n", argv[0]);
+		exit(1);
+	}
 
 /* Stockage et conversion des arguments */
 ip_serveur = argv[1];
@@ -69,8 +69,8 @@ port = atoi(argv[2]);
 
 /* Création de la socket TCP */
 if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-perror("Erreur lors de la création du socket TCP\n");
-exit(1);
+	perror("Erreur lors de la création du socket TCP\n");
+	exit(1);
 }
 
 /* Structure pour le bind */
@@ -79,37 +79,30 @@ serveur.sin_port = htons(port);
 serveur.sin_addr.s_addr = inet_addr(ip_serveur);
 
 if (connect(sock, (struct sockaddr *) &serveur, serveur_len) < 0) {
-perror("Erreur de connection au serveur");
-exit(1);
+	perror("Erreur de connection au serveur");
+	exit(1);
 }
 
 printf("Vous êtes connecté");
-
- buffer = (unsigned char *) calloc (BUF_LEN, sizeof(unsigned char));
-
+buffer = (unsigned char*)calloc(BUF_LEN, sizeof(unsigned char));
 while(1) {
 
-/* Initialisation du buffer */
-for(i = 0; i < BUF_LEN-1; i++) {
-buffer[i] = '\0';
+	/* Initialisation du buffer */
+	for(i = 0; i < BUF_LEN-1; i++) {
+		buffer[i] = '\0';
+	}
+
+	buffer[BUF_LEN-1]='\n';	
+
+	/* On lis le message entrée par le client sur la console */
+	if(lire(buffer,BUF_LEN) == 1) {
+		perror("Erreur lors de la lecture du message envoyée par le client");
+		exit(1);
+	}
+
+	/* On envoie le message qui a été écrit par le client */
+	write(sock, buffer, BUF_LEN);
 }
-
-buffer[BUF_LEN-1]='\n';	
-
-/* On lis le message entrée par le client sur la console */
-if(lire(buffer,BUF_LEN) == 1) {
-perror("Erreur lors de la lecture du message envoyée par le client");
-exit(1);
-}
-
- printf("123\n");
-
-/* On envoie le message qui a été écrit par le client */
-write(sock, buffer, BUF_LEN);
- printf("4556\n");
-}
-
- printf("djqzk\n");
 
 close(sock);
 
