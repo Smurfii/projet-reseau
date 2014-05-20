a321de9624821915dc4761983652a4c50d326684
Smurfii <hugo.durchon@gmail.com>
2014-05-20 09:43:57 +0200

    Update client_projet.c

--- client_projet.c
+++ client_projet.c
@@ -42,6 +42,7 @@ int main(int argc, char ** argv){
 /* Arguments */
 char * ip_serveur;
 int port;
+char * nom_fichier;
 
 /* Variables pour la création de la socket TCP */
 
@@ -51,14 +52,23 @@ socklen_t serveur_len, client_len;
 
 /*Variables de stockage et autre */
 char * buffer;
-int /*ret, f,*/ i;
+int x, fd, i, nboct;
+
+/* On ouvre le fichier en lecture seule*/
+fd = open(nom_fichier, O_RDONLY);
+if(fd == -1) {
+    perror("open");
+    exit(1);
+}
+printf("Fichier %s ouvert...\n", nom_fichier);
+
 
 /*Valeurs par défaut des tailles des clients */
 serveur_len = sizeof(serveur);
 client_len = sizeof(client);
 
 /* Vérification du nombre d'arguments */
-	if (argc != 3) {
+	if (argc != 4) {
 		fprintf(stderr, "Usage: %s ip_serveur, nom_fichier, port\n", argv[0]);
 		exit(1);
 	}
@@ -66,6 +76,7 @@ client_len = sizeof(client);
 /* Stockage et conversion des arguments */
 ip_serveur = argv[1];
 port = atoi(argv[2]);
+nom_fichier = argv[3];
 
 /* Création de la socket TCP */
 if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -84,7 +95,38 @@ if (connect(sock, (struct sockaddr *) &serveur, serveur_len) < 0) {
 }
 
 printf("Vous êtes connecté");
+
+  // Fonction send : permet d'envoyer des données lors d'une connexion TCP.
+  // arg 1 : fd du socket
+  // arg 2 : pointeur vers le buffer de données
+  // arg 3 : nombre d'octets à envoyer
+  // arg 4 : flags, ici 0
+  
+// On envoie la longueur du nom du fichier
+x = strlen(nom_fichier);
+if(send(sock, &x, sizeof(int), 0) == -1){
+    perror("send");
+    close(sock);
+    close(fd);
+    exit(1);
+}
+printf("Debut envoi du nom de fichier...\n");
+  
+// Envoi du nom du fichier
+nboct = send(sock, nom_fichier, x, 0);
+if(nboct == -1){
+    perror("send");
+    close(sock);
+    close(fd);
+    exit(1);
+}
+printf("Nom fichier envoyé...\n");
+  
+printf("Debut envoi du fichier...\n");
+  
+/* Il faut allouer le buffer d'envoi de données */
 buffer = (unsigned char*)calloc(BUF_LEN, sizeof(unsigned char));
+  
 while(1) {
 
 	/* Initialisation du buffer */
