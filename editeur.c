5f9ae0b3922eefad69ce32f62eb60c53b39b7098
Foufoune <foucauld.bouvattier@gmail.com>
2014-05-27 00:44:47 +0200

    Update editeur.c

--- editeur.c
+++ editeur.c
@@ -1,116 +1,115 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <signal.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <errno.h>
-#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#define TAILLE_MAX 80
 
-#define BUF_LEN 256
+// fonction qui concatène deux entiers : intcat(42, 666) renverra 42666
+int intcat(int v1, int v2){
+    /* On récupère le nombre de chiffres du premier nombre */
+    int n = floor(log10(v1) + 1);
+    /* On mulitplie le premier nombre par 10^n et on additionne le second */
+    int result = v1 * pow(10, n) + v2;
+    return result;
+}
 
+// fonction qui décale tous les caractères d'un fichier de 1 à partir de la position du curseur et écrit "_" à la position initiale du curseur
+void decaler(int position_curseur, FILE* fichier){
+	int i=0;
+	fseek(fichier, 0, SEEK_END);
+	int fin=ftell(fichier);
+	fseek(fichier, position_curseur, SEEK_SET);
+	int size = fin - position_curseur;
+	int buf[size];
+	int caractere = 0;
+	while(!feof(fichier))
+	{
+		caractere = fgetc(fichier);
+		printf("%d", caractere);
+		buf[i] = caractere;
+		i=i+1;
+	}
+	fseek(fichier, position_curseur, SEEK_SET);
+	fputc('_', fichier);
+	for(i=0; i<size; i=i+1)
+	{
+		fputc(buf[i], fichier);
+	}
+	fseek(fichier, position_curseur, SEEK_SET);
+}
 
-int lire(char *chaine, int longueur)
-{	
-/*Initialisation de la position d'entrée */
-    char *positionEntree = NULL;
-  
-    /* On lit le texte saisi au clavier */
-    if (fgets(chaine, longueur, stdin) != NULL) { /* Si la saisie se fait sans erreur : */
-        
-        positionEntree = strchr(chaine, '\n'); /* On recherche l'Entrée" */
-        
-        if (positionEntree != NULL) { /* Si on a trouvé le retour à la ligne */
+//fonction qui affiche ce qu'il y a d'écrit dans un fichier
+void afficher(FILE* fichier){
+  int position_curseur = ftell(fichier);
+  char chaine[TAILLE_MAX] = "";
+  fseek(fichier, 0, SEEK_SET);
+  if (fichier != NULL)
+    {
+      printf("\n");
       
-            *positionEntree = '\0'; /* On remplace ce caractère par \0 */
-        }
-
-        return 0; /* On renvoie 0 si la fonction s'est déroulée sans erreur */
+      while (fgets(chaine, TAILLE_MAX, fichier) != NULL) /*On lit maximum TAILLE_MAX caractères du fichier, on stocke le tout dans "chaine"*/
+      {
+      printf("%s", chaine); /*on affiche la chaine*/
+      }
+      printf("\n");
+      
     }
+    printf("\n");
+    fseek(fichier, position_curseur, SEEK_SET);
+}
 
-    else {
-        return 1; /* On renvoie 1 si la fonction s'est mal déroulée */
+//fonction qui écrit une lettre dans un fichier
+void ecrire(char lettre, FILE* fichier, int *position_curseur){
+	decaler(*position_curseur, fichier);
+	fputc(lettre, fichier);
+	*position_curseur = *position_curseur + 1;
+}
+
+void supprimer(FILE* fichier, int *position_curseur)
+{
+
+}
+
+void coucou(char* fichier){
+	int position_curseur;
+	char nom_fichier[TAILLE_MAX];
+	strcpy(nom_fichier, fichier);
+	char* nom_curseur = strcat(nom_fichier, "_curseur");
+	FILE* fdc = fopen(nom_curseur, "r+");
+	FILE* fd = fopen(fichier, "r+");
+	fscanf(fdc, "%d", &position_curseur);
+	fclose(fdc); //on ferme le fichier
+	fdc = fopen(nom_curseur, "w+"); //on réouvre le fichier en écrasant l'ancienne position du curseur.
+	printf("%d", position_curseur);
+	char lettre;
+	if(fd==NULL)
+	{
+		perror("fopen_fichier");
+	}
+	afficher(fd);
+	printf("écrivez votre texte\n");
+	while(lettre!='$')
+	{
+		if(lettre=='$')
+			{break;}
+		lettre = getchar();
+		ecrire(lettre, fd, &position_curseur);
+	}
+	fclose(fd);
+	fprintf(fdc, "%d", position_curseur);
+}
+
+int main(int argc, char **argv)
+{
+	coucou(argv[1]);
+    if(argc != 2)
+    {   
+        perror("mauvais nombre d'arguments");
+        return 1;
     }
-}
-
-int main(int argc, char ** argv){
-
-/* Arguments */
-char * ip_serveur, * nom_fichier;
-int port;
-
-/* Variables pour la création de la socket TCP */
-
-int sock;
-struct sockaddr_in serveur, client;
-socklen_t serveur_len, client_len;
-
-/*Variables de stockage et autre */
-char buffer[BUF_LEN];
-int ret, f, i;
-
-/*Valeurs par défaut des tailles des clients */
-serveur_len = sizeof(serveur);
-client_len = sizeof(client);
-
-/* Vérification du nombre d'arguments */
-if (argc != 4) {
-fprintf(stderr, "Usage: %s ip_serveur, nom_fichier, port\n", argv[0]);
-exit(1);
-}
-
-/* Stockage et conversion des arguments */
-ip_serveur = argv[1];
-port = atoi(argv[2]);
-nom_fichier = argv[3];
-
-/* Création de la socket TCP */
-if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-perror("Erreur lors de la création de la socket TCP\n");
-exit(1);
-}
-
-/* Structure pour le bind */
-serveur.sin_family = AF_INET;
-serveur.sin_port = htons(port);
-serveur.sin_addr.s_addr = inet_addr(ip_serveur);
-
-if (connect(sock, (struct sockaddr *) &serveur, serveur_len) < 0) {
-perror("Erreur de connection au serveur");
-exit(1);
-}
-
-/* Initialisation du buffer */
-for(i = 0; i < BUF_LEN; i++)
-{
-buffer[i] = '\0';
-}
-
-/* On lis le message entrée par le client sur la console */
-if(lire(buffer,BUF_LEN) == 1) {
-perror("Erreur lors de la lecture du message envoyée par le client");
-exit(1);
-}
-
-/* On envoie le message qui a été écrit par le client */
-write(sock, buffer, BUF_LEN);
-
-
-/*f = open(nom_fichier, O_RDONLY);
-
-while((ret = read(f, buffer, sizeof buffer)) > 0){
-if(write(sock, buffer, ret) < 1)
-{
-perror("Erreur d'envoi au serveur");
-exit(1);
-}
-}*/
-
-close(sock);
-
-return 0;
+	return 0;
 }
